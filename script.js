0e5ad8439af1e8a655fff1e9477af7d250736cd8
Reza Ramadhan <97609184+rezarmdn@users.noreply.github.com>
2022-12-11 08:32:35 +0700

    tugas uts menghitung diskon - html,css,js

---
+++ script.js
@@ -0,0 +1,19 @@
+ 
+function hitung() {  
+  var harga= parseFloat(document.hitungdiskon.harga.value);  
+  if (isNaN(harga)) {harga=0.0;}  
+
+  var diskon=parseFloat(document.hitungdiskon.diskon.value);  
+  if (isNaN(diskon)){diskon=0.0;}var hargadiskon= harga * diskon / 100 ;
+
+  const harga1= parseFloat(document.hitungdiskon.diskon.value);
+  const polaregex= /^[0-9]{1,2}$/  
+  if (polaregex.test(harga1)){} 
+    else{
+      alert("harga dan diskon tidak boleh kosong ,  100% atau melebihi 100%");
+      input.harga1=0;
+  }
+
+  
+  var hasil = harga - hargadiskon;  document.hitungdiskon.hasildiskon.value= hasil;  document.hitungdiskon.hrgdiskon.value= hargadiskon; 
+}
